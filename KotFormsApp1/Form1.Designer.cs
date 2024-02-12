0127a3b2bf810d496f01e6954575fff4e62ce366
RPG512 <maxim-kotenko@hotmail.com>
2024-02-12 21:04:18 +0300

    Добавьте файлы проекта.

---
+++ KotFormsApp1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace KotFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.textBoxCostPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCosts = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPercent = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxCount = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Название";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(13, 30);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(200, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(13, 57);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 2;
+            this.button.Text = "Анализ";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // textBoxCostPrice
+            // 
+            this.textBoxCostPrice.Location = new System.Drawing.Point(225, 30);
+            this.textBoxCostPrice.Name = "textBoxCostPrice";
+            this.textBoxCostPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCostPrice.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Себестоимость";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(331, 30);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPrice.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(328, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Цена продажи";
+            // 
+            // textBoxCosts
+            // 
+            this.textBoxCosts.Location = new System.Drawing.Point(437, 30);
+            this.textBoxCosts.Name = "textBoxCosts";
+            this.textBoxCosts.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCosts.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(434, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Издержки";
+            // 
+            // textBoxPercent
+            // 
+            this.textBoxPercent.Location = new System.Drawing.Point(649, 30);
+            this.textBoxPercent.Name = "textBoxPercent";
+            this.textBoxPercent.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPercent.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(646, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "% возврата";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 86);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Баланс";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(960, 463);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "$";
+            // 
+            // textBoxCount
+            // 
+            this.textBoxCount.Location = new System.Drawing.Point(543, 30);
+            this.textBoxCount.Name = "textBoxCount";
+            this.textBoxCount.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCount.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(540, 13);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Кол-во в партии";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxCount);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxPercent);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxCosts);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCostPrice);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 600);
+            this.MinimumSize = new System.Drawing.Size(1000, 600);
+            this.Name = "Form1";
+            this.Text = "KotenkoApp";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.TextBox textBoxCostPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCosts;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPercent;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxCount;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ KotFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KotFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            Random random = new Random();
+            double balanse = 0;
+            double[] doubles = new double[5];
+            TextBox[] names = new TextBox[5] { textBoxCostPrice, textBoxPrice, textBoxCosts, textBoxCount, textBoxPercent };
+            bool conv = false;
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i].Text == "")
+                {
+                    conv = false;
+                    break;
+                }
+                else
+                    conv = true;
+            }
+            if (conv)
+            {
+                for (int i = 0; i < names.Length; i++)
+                    doubles[i] = Convert.ToDouble(names[i].Text);
+                while (doubles[3] > 0)
+                {
+                    if (random.Next(101) > doubles[4])
+                    {
+                        balanse += doubles[1] - doubles[0] - doubles[2];
+                        doubles[3]--;
+                    }
+                    else
+                        balanse -= doubles[2];
+                    chart1.Series[0].Points.Add(balanse);
+                }
+            }
+            else
+                MessageBox.Show("Пожалуйста введите все значения!");
+            
+        }
+    }
+}
